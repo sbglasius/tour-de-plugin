1128f4f1a2027a3eb43e42ce5d211b35a2bca865
Soeren Berg Glasius <soeren@glasius.dk>
2014-07-19 22:51:15 +0200

    Stage 1: Create data from Tour De France Homepage

---
+++ grails-app/domain/tour/Rider.groovy
@@ -0,0 +1,18 @@
+package tour
+
+class Rider {
+    String name
+    String image
+    Integer weight
+    Date born
+
+    String toString() {
+        name
+    }
+
+    static constraints = {
+        weight nullable: true
+    }
+
+    static belongsTo = [team: Team]
+}
